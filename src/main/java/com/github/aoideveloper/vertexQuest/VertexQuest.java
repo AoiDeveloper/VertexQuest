d85d461156c39302ec4c11f750a9fbfcd80d2194
MakiZushi <aoibotowner@gmail.com>
2025-06-12 19:13:26 +0900

    initial commit

---
+++ src/main/java/com/github/aoideveloper/vertexQuest/VertexQuest.java
@@ -0,0 +1,16 @@
+package com.github.aoideveloper.vertexQuest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VertexQuest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+   }
+}
